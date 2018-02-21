40ac635e1cc556887acccf7b5f4bda7361e74a68
Zoe Cesar <zoe.a.cesar@gmail.com>
2018-02-21 11:48:18 -0500

    updated the help page
    added styles and string file for help page for organization. Imported arvo font

--- app/src/main/java/com/example/dejatjackson/pybot/HelpActivity.java
+++ app/src/main/java/com/example/dejatjackson/pybot/HelpActivity.java
@@ -11,56 +11,5 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-
-        TextView textView = findViewById(R.id.text_view_id);
-        textView.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView1 = findViewById(R.id.textView);
-        textView1.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView2 =  findViewById(R.id.textView1);
-        textView2.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView3 = findViewById(R.id.textView2);
-        textView3.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView4 = findViewById(R.id.textView3);
-        textView4.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView5 = findViewById(R.id.textView4);
-        textView5.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView6 = findViewById(R.id.textView5);
-        textView6.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView7 = findViewById(R.id.textView6);
-        textView7.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView8 = findViewById(R.id.textView7);
-        textView8.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView9 = findViewById(R.id.textView8);
-        textView9.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView10 = findViewById(R.id.textView9);
-        textView10.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView11 = findViewById(R.id.textView10);
-        textView11.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView12 = findViewById(R.id.textView11);
-        textView12.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView13 = findViewById(R.id.textView12);
-        textView13.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView14 = findViewById(R.id.textView13);
-        textView14.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView15 = findViewById(R.id.textView14);
-        textView15.setMovementMethod(new ScrollingMovementMethod());
-
-        TextView textView16 = findViewById(R.id.textView15);
-        textView16.setMovementMethod(new ScrollingMovementMethod());
     }
 }
